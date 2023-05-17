aeae4eb61e4fde448101f7bb402b0b0ded3e06ae
Jia-A <agrawaljiya2011@gmail.com>
2023-05-17 11:46:41 +0530

    variables-lesson

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
-let myDiv = document.querySelector(".aashu")
+var y = 10 //declaration, initialization, assignment
 
-myDiv.addEventListener("click", ()=> console.log("hello"))
+// var name = "Aashu"
+// var x = true
+// var c = 10
+// console.log(y % 2 == 0)
+
+
+var v //declaration
+v = 20 //initialization and assignment
+v = 40 //reassignment
+console.log("Value of V in line 11 : ",v)
+
+var v //redeclaration of v which should not be allowed
+console.log("New V's value in line 15 : ",v)
+
+var r = v-20  //because v is redeclared, v's new value now is 90
+console.log(r)
+
+let p = 30
+// let p = 30
+
+const w = 10
+
+var homeAddress = "Home Address"
+var home_address = 8
+
+let x = 10 
+//These both are different
+let X = 10
+
+//Difference between var, let and const
+
+// 1. var variables can be redeclared, reassigned and can be initialised later on.
+// 2. let variables can be reassigned but not redecalred and can be initialised later on.
+// 3. const variables cannot be reassigned, redeclared and needs to be initialised at the time of declaration.
+
+//Extra points about var, let and const
+// initialization can only happen once.
+// You should not use var to declare variables.
+// Try to use const as much as possible otherwise use let.
+
+// Naming conventions
+// ALL VARIABLES ARE CASE SENSITIVE means var x is not equals to var X.
+// There can be no spacing between names of the variables.
+// Variable name can start with _, $, or a letter.
+// Variable name cannot start with a number, but can end in it.
+// Camel Casing : homeAddress, jiyaHouse
+// Snake Casing : home_address, jiya_House
+
+
+
+
+
+
+
+
+
